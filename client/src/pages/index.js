cf5517b41122498bd3e7201c95779377324a0d30
gadez1992 <61583419+gadez1992@users.noreply.github.com>
2021-02-17 15:29:54 -0500

    added login route and component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,10 +2,12 @@ import ItemInsert from './ItemInsert';
 import ItemsList from './ItemsList';
 import ItemsTable from './ItemsTable';
 import ItemUpdate from './ItemUpdate';
+import Login from './Login';
 
 export {
     ItemInsert,
     ItemsList,
     ItemsTable,
-    ItemUpdate
+    ItemUpdate,
+    Login,
 };
